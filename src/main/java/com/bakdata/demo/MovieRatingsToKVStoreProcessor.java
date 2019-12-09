06edbf9f5f60362da06e16dd2c30030f60ee5ab9
jak-ing <lupin2702@gmail.com>
2019-12-09 12:33:19 +0100

    Send ratings to new topic keyed by userId to build ratingsForUsers later

--- src/main/java/com/bakdata/demo/MovieRatingsToKVStoreProcessor.java
+++ src/main/java/com/bakdata/demo/MovieRatingsToKVStoreProcessor.java
@@ -1,18 +1,32 @@
 package com.bakdata.demo;
 
-import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.processor.Processor;
 import org.apache.kafka.streams.processor.ProcessorContext;
 import org.apache.kafka.streams.processor.ProcessorSupplier;
-import org.apache.kafka.streams.processor.PunctuationType;
-import org.apache.kafka.streams.state.KeyValueIterator;
 import org.apache.kafka.streams.state.KeyValueStore;
 
 import java.nio.ByteBuffer;
-import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Properties;
 
 public final class MovieRatingsToKVStoreProcessor {
+    static Properties getStreamsConfig() {
+        final Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-wordcount");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+
+        // setting offset reset to earliest so that we can re-run the demo code with the same pre-loaded data
+        // Note: To re-run the demo, you need to use the offset reset tool:
+        // https://cwiki.apache.org/confluence/display/KAFKA/Kafka+Streams+Application+Reset+Tool
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        return props;
+    }
 
     static class MyProcessorSupplier implements ProcessorSupplier<String, String> {
 
@@ -26,23 +40,14 @@ public final class MovieRatingsToKVStoreProcessor {
                 @SuppressWarnings("unchecked")
                 public void init(final ProcessorContext context) {
                     this.context = context;
-                    this.context.schedule(Duration.ofSeconds(1), PunctuationType.STREAM_TIME, timestamp -> {
-                        try (final KeyValueIterator<Integer, ByteBuffer> iter = kvStore.all()) {
-                            while (iter.hasNext()) {
-                                final KeyValue<Integer, ByteBuffer> entry = iter.next();
-                                context.forward(entry.key, entry.value);
-                            }
-                        }
-                    });
-                    this.kvStore = (KeyValueStore<Integer, ByteBuffer>) context.getStateStore("ratingsForMovies");
+                    this.kvStore = (KeyValueStore<Integer, ByteBuffer>) this.context.getStateStore("ratingsForMovies");
                 }
 
                 @Override
-                public void process(final String dummy, final String ratingsForOneMovie) {
-                    int movieId = Integer.parseInt(ratingsForOneMovie.split(":")[0]);
-
+                public void process(final String movieId, final String ratingsForOneMovie) {
+                    // phase 1: write movieId rating vectors to store for **movie perspective**
                     ArrayList<Integer> ratings = new ArrayList<>();
-                    for (String userIdRatingPair : ratingsForOneMovie.split(":")[1].split(";")) {
+                    for (String userIdRatingPair : ratingsForOneMovie.split(";")) {
                         ratings.add(Integer.parseInt(userIdRatingPair.split(",")[1]));
                     }
 
@@ -51,8 +56,19 @@ public final class MovieRatingsToKVStoreProcessor {
                         bb.putInt(rating);
                     }
 
-                    this.kvStore.put(movieId, bb);
-                    context.commit();
+                    this.kvStore.put(Integer.parseInt(movieId), bb);
+
+                    // phase 2: turn around ratings to have userId,movieId,rating triples for **user perspective**
+                    for (String userIdRatingPair : ratingsForOneMovie.split(";")) {
+                        this.context.forward(
+                            userIdRatingPair.split(",")[0],
+                            movieId + "," + userIdRatingPair.split(",")[1]
+                        );
+                    }
+
+                    // phase 3: commit
+                    // TODO: commit periodically rather than after every record for better performance?
+                    this.context.commit();
                 }
 
                 @Override
--- src/test/java/com/bakdata/demo/MovieRatingsToKVStoreProcessorTest.java
+++ src/test/java/com/bakdata/demo/MovieRatingsToKVStoreProcessorTest.java
@@ -1,28 +1,86 @@
 package com.bakdata.demo;
 
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.LongDeserializer;
 import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.TopologyTestDriver;
 import org.apache.kafka.streams.processor.MockProcessorContext;
 import org.apache.kafka.streams.processor.Processor;
 import org.apache.kafka.streams.state.KeyValueStore;
+import org.apache.kafka.streams.state.StoreBuilder;
 import org.apache.kafka.streams.state.Stores;
+import org.apache.kafka.streams.test.ConsumerRecordFactory;
+import org.apache.kafka.streams.test.OutputVerifier;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.nio.ByteBuffer;
-import java.util.Iterator;
 
 import static org.junit.Assert.*;
 
 public class MovieRatingsToKVStoreProcessorTest {
+    private StoreBuilder<KeyValueStore<Integer, ByteBuffer>> storeSupplier;
+    private TopologyTestDriver testDriver;
+    private StringDeserializer stringDeserializer = new StringDeserializer();
+    private LongDeserializer longDeserializer = new LongDeserializer();
+    private ConsumerRecordFactory<String, String> recordFactory = new ConsumerRecordFactory<>(new StringSerializer(), new StringSerializer());
+
+    @Before
+    public void setup() {
+        Topology builder = new Topology();
+
+        this.storeSupplier = Stores.keyValueStoreBuilder(
+                Stores.inMemoryKeyValueStore("ratingsForMovies"),
+                Serdes.Integer(),
+                Serdes.ByteBuffer()
+        ).withLoggingDisabled();  // Changelog is not supported by MockProcessorContext.
+
+        builder.addSource("Source", "movieIds-with-ratings")
+                .addProcessor("Process", () -> new MovieRatingsToKVStoreProcessor.MyProcessorSupplier().get(), "Source")
+                .addStateStore(storeSupplier, "Process")
+                .addSink("Sink", "userId-movieId-rating-triple", "Process");
+
+        testDriver = new TopologyTestDriver(builder, MovieRatingsToKVStoreProcessor.getStreamsConfig());
+    }
+
+    @After
+    public void tearDown() {
+        try {
+            testDriver.close();
+        } catch (final RuntimeException e) {
+            System.out.println("Ignoring exception, test failing in Windows due this exception:" + e.getLocalizedMessage());
+        }
+    }
+
     @Test
-    public void test() {
+    public void testTopology() {
+        testDriver.pipeInput(recordFactory.create("movieIds-with-ratings", "3", "1,5;2,4"));
+
+        ProducerRecord<String, String> outputRecord1 = testDriver.readOutput(
+                "userId-movieId-rating-triple",
+                stringDeserializer,
+                stringDeserializer);
+        OutputVerifier.compareKeyValue(outputRecord1, "1", "3,5");
+
+        ProducerRecord<String, String> outputRecord2 = testDriver.readOutput(
+                "userId-movieId-rating-triple",
+                stringDeserializer,
+                stringDeserializer);
+        OutputVerifier.compareKeyValue(outputRecord2, "2", "3,4");
+
+        assertNull(testDriver.readOutput("userId-movieId-rating-triple", stringDeserializer, longDeserializer));
+    }
+
+    @Test
+    public void testProcessor() {
         final MockProcessorContext context = new MockProcessorContext();
 
         // Create, initialize, and register the state store.
-        final KeyValueStore<Integer, ByteBuffer> store =
-                Stores.keyValueStoreBuilder(Stores.inMemoryKeyValueStore("ratingsForMovies"), Serdes.Integer(), Serdes.ByteBuffer())
-                        .withLoggingDisabled() // Changelog is not supported by MockProcessorContext.
-                        .build();
+        final KeyValueStore<Integer, ByteBuffer> store = this.storeSupplier.build();
         store.init(context, store);
         context.register(store, null);
 
@@ -31,30 +89,17 @@ public class MovieRatingsToKVStoreProcessorTest {
         processor.init(context);
 
         // send a record to the processor
-        // TODO: use real key to use multiple partitions
-        processor.process("key", "3:1,5;2,4");
+        processor.process("3", "1,5;2,4");
 
-        // note that the processor commits, but does not forward, during process()
         assertTrue(context.committed());
-        assertTrue(context.forwarded().isEmpty());
 
-        // now, we trigger the punctuator, which iterates over the state store and forwards the contents.
-        context.scheduledPunctuators().get(0).getPunctuator().punctuate(0L);
-
-        // finally, we can verify the output.
-        final Iterator<MockProcessorContext.CapturedForward> capturedForwards = context.forwarded().iterator();
-        // TODO: question: these captured forwards come from the Processor > init > schedule ?!
-        KeyValue<Integer, ByteBuffer> capturedKV = capturedForwards.next().keyValue();
-
-        assertEquals(3, (long) capturedKV.key);
+        assertNotNull(store.get(3));
+        ByteBuffer processedValue = store.get(3);
 
         int[] ratings = new int[] {5, 4};
-
         // += 4 because we're iterating over a byte[] and not an int[]
-        for (int i = 0; i < capturedKV.value.capacity(); i += 4) {
-            assertEquals(capturedKV.value.getInt(i), ratings[i/4]);
+        for (int i = 0; i < processedValue.capacity(); i += 4) {
+            assertEquals(processedValue.getInt(i), ratings[i/4]);
         }
-
-        assertFalse(capturedForwards.hasNext());
     }
 }
